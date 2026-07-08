c5e427bb666b6d342387a897ce14dfef50452375
al <abduldeveloper30@gmail.c9m>
2026-07-08 07:26:18 +0530

    Use relative API URL

--- client/src/api/community.ts
+++ client/src/api/community.ts
@@ -3,30 +3,24 @@ import axios from "axios";
 import type { Subreddit } from "@/types/community";
 
 export const api = axios.create({
-
-    baseURL: "http://localhost:3000",
-
+    baseURL: "/api",
 });
 
 export async function getSubreddit() {
-
     const { data } = await api.get<Subreddit>(
-        "/subreddit"
+        "/subreddit",
     );
 
     return data;
-
 }
 
 export async function updateSubreddit(
     subreddit: Subreddit,
 ) {
-
     const { data } = await api.put<Subreddit>(
         "/subreddit",
         subreddit,
     );
 
     return data;
-
 }
